b4fc9bda65d798f565f6b2170476332f345420cc
rinjugatla <rin.jugatla@gmail.com>
2024-02-29 19:00:39 +0900

    未使用のusingを削除

--- PalWorld RCON GUI/Rcon.cs
+++ PalWorld RCON GUI/Rcon.cs
@@ -4,8 +4,6 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Collections.Generic;
-using System.Security.Permissions;
 
 namespace PalWorldR
 {
