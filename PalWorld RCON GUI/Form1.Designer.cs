9eda0e62efa613acfbb71dd0be32bcf3828b8a58
ktti <m0a211850f@edu.teu.ac.jp>
2024-03-19 16:34:47 +0900

    ウィンドウサイズの固定化

--- PalWorld RCON GUI/Form1.Designer.cs
+++ PalWorld RCON GUI/Form1.Designer.cs
@@ -495,6 +495,8 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "PalWorld RCON GUI";
+            this.MaximumSize = this.Size;
+            this.MinimumSize = this.Size;
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
